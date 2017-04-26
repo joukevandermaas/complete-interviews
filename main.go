ffcd0fa382b50e4fdd153d60e3415ce6c2c6e99f
Jouke van der Maas <jouke.vandermaas@gmail.com>
2017-04-26 23:46:39 +0200

    get infrastructure there (without parsing)

--- main.go
+++ main.go
@@ -1,7 +1,80 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+type pageContent struct {
+	body string
+	url  string
+}
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	url := "https://rc-interviewing.niposoftware-dev.com/Interviews/wwuaw/NKtRYwUmedoyizz48yiU"
+
+	chInterviews := make(chan pageContent)
+
+	go getContent(url, chInterviews)
+	result := <-chInterviews
+
+	questionNumber := 0
+	hasAnotherQuestion := true // depends on result.url
+
+	for hasAnotherQuestion {
+		newRequest := getInterviewResponse(result.body, questionNumber)
+		go postContent(result.url, newRequest, chInterviews)
+		result = <-chInterviews
+
+		questionNumber++
+		hasAnotherQuestion = false // depends on result.url
+	}
+
+	os.Exit(0)
+}
+
+func getInterviewResponse(html string, questionNumber int) url.Values {
+	fmt.Print(html)
+	// parse html, find question & possible answers and form result
+	return nil
+}
+
+func postContent(url string, body url.Values, ch chan pageContent) {
+	response, err := http.Post(url, "application/x-www-form-urlencoded", strings.NewReader(body.Encode()))
+	if err != nil {
+		ch <- pageContent{}
+	} else {
+		defer response.Body.Close()
+
+		url := response.Request.URL.String()
+
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(response.Body)
+		str := buf.String()
+
+		result := pageContent{body: str, url: url}
+		ch <- result
+	}
+}
+
+func getContent(url string, ch chan pageContent) {
+	response, err := http.Get(url)
+	if err != nil {
+		ch <- pageContent{}
+	} else {
+		defer response.Body.Close()
+
+		url := response.Request.URL.String()
+
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(response.Body)
+		str := buf.String()
+
+		result := pageContent{body: str, url: url}
+		ch <- result
+	}
 }
