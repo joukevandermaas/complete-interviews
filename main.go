a32b09654a393b13f019789bff8ee6ca6a68394c
Jouke van der Maas <jouke.vandermaas@gmail.com>
2017-04-27 00:24:18 +0200

    clean up a bit

--- main.go
+++ main.go
@@ -21,13 +21,25 @@ func main() {
 	// "https://rc-interviewing.niposoftware-dev.com/Interviews/wwuaw/NKtRYwUmedoyizz48yiU"
 	url := os.Args[1]
 
+	chInterview := make(chan error)
+
+	go performInterview(url, chInterview)
+
+	result := <-chInterview
+
+	if result != nil {
+		log.Fatal(result)
+	}
+}
+
+func performInterview(url string, ch chan error) {
 	chInterviews := make(chan pageContent)
 
 	go getContent(url, chInterviews)
 	result := <-chInterviews
 
 	if result.err != nil {
-		log.Fatal(result.err)
+		ch <- result.err
 	}
 
 	questionNumber := 0
@@ -40,12 +52,14 @@ func main() {
 		result = <-chInterviews
 
 		if result.err != nil {
-			log.Fatal(result.err)
+			ch <- result.err
 		}
 
 		questionNumber++
 		hasAnotherQuestion = false // depends on result.url
 	}
+
+	ch <- nil
 }
 
 func getInterviewResponse(html string, questionNumber int) url.Values {
@@ -56,42 +70,34 @@ func getInterviewResponse(html string, questionNumber int) url.Values {
 
 func postContent(url string, body url.Values, ch chan pageContent) {
 	response, err := http.Post(url, "application/x-www-form-urlencoded", strings.NewReader(body.Encode()))
-	if err != nil {
-		ch <- pageContent{err: err}
-	} else {
 
-		defer response.Body.Close()
-
-		url := response.Request.URL.String()
-
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(response.Body)
-		str := buf.String()
-
-		result := pageContent{body: str, url: url}
-
-		if response.StatusCode >= 400 {
-			result.err = fmt.Errorf("http request was unsuccessful: %s", response.Status)
-		}
-
-		ch <- result
-	}
+	ch <- handleHTTPResult(response, err)
 }
 
 func getContent(url string, ch chan pageContent) {
 	response, err := http.Get(url)
+
+	ch <- handleHTTPResult(response, err)
+}
+
+func handleHTTPResult(response *http.Response, err error) pageContent {
 	if err != nil {
-		ch <- pageContent{}
-	} else {
-		defer response.Body.Close()
-
-		url := response.Request.URL.String()
-
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(response.Body)
-		str := buf.String()
-
-		result := pageContent{body: str, url: url}
-		ch <- result
+		return pageContent{err: err}
 	}
+
+	defer response.Body.Close()
+
+	url := response.Request.URL.String()
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(response.Body)
+	str := buf.String()
+
+	result := pageContent{body: str, url: url}
+
+	if response.StatusCode >= 400 {
+		result.err = fmt.Errorf("http request was unsuccessful: %s (url: %s)", response.Status, url)
+	}
+
+	return result
 }
