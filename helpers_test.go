c0ed0f4823da57d0968e34ead86b9b11fb3bb47e
Jouke van der Maas <jouke.vandermaas@gmail.com>
2017-05-22 22:16:59 +0200

    Test both chicago and default template

--- helpers_test.go
+++ helpers_test.go
@@ -4,9 +4,65 @@ import (
 	"io/ioutil"
 	"net/url"
 	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"path/filepath"
 
 	"golang.org/x/net/html"
 )
+
+var templates = []string{"default", "chicago"}
+
+var pageToQtype = map[string]string{
+	"welcome-page": qTypePage,
+	"alpha-single": qTypeOpenSingle,
+	"multi-coded":  qTypeCategory,
+	"number":       qTypeNumber,
+	"open-multi":   qTypeOpenMulti,
+	"single-coded": qTypeCategory,
+}
+
+func stringForBothTemplates(t *testing.T, page string, test func(string)) {
+	for _, template := range templates {
+		fileName := filepath.Join("pages", template, page+".html")
+
+		html, err := getHTMLString(fileName)
+		assert.NoError(t, err)
+
+		t.Logf("Testing filename '%s'", fileName)
+		test(html)
+	}
+}
+
+func stringForAllQuestionTypes(t *testing.T, test func(string, string)) {
+	for page, questionType := range pageToQtype {
+		stringForBothTemplates(t, page, func(node string) {
+			test(node, questionType)
+		})
+	}
+}
+
+func forBothTemplates(t *testing.T, page string, test func(*html.Node)) {
+	stringForBothTemplates(t, page, func(html string) {
+		doc, err := htmlStringToNode(html)
+
+		assert.NoError(t, err)
+
+		test(doc)
+	})
+}
+
+func forAllQuestionTypes(t *testing.T, test func(*html.Node, string)) {
+	stringForAllQuestionTypes(t, func(html string, questionType string) {
+		doc, err := htmlStringToNode(html)
+
+		assert.NoError(t, err)
+
+		test(doc, questionType)
+	})
+}
 
 func htmlStringToNode(snippet string) (*html.Node, error) {
 	doc, err := html.Parse(strings.NewReader(snippet))
--- questions_test.go
+++ questions_test.go
@@ -5,6 +5,8 @@ import (
 	"net/url"
 	"testing"
 
+	"golang.org/x/net/html"
+
 	"strconv"
 
 	"github.com/stretchr/testify/assert"
@@ -13,268 +15,230 @@ import (
 func TestSetOpenMultiQuestionValues(t *testing.T) {
 	assert := assert.New(t)
 
-	doc, err := getHTMLDocument("test-files/multiline.html")
-	assert.NoError(err)
+	forBothTemplates(t, "open-multi", func(doc *html.Node) {
+		values := make(url.Values)
 
-	values := make(url.Values)
+		err := setOpenMultiQuestionValues(doc, values)
+		assert.NoError(err)
 
-	err = setOpenMultiQuestionValues(doc, values)
-	assert.NoError(err)
+		result := flattenURLValues(values)
 
-	result := flattenURLValues(values)
+		t.Logf("%v\n", result)
 
-	t.Logf("%v\n", result)
-
-	assert.NotEmpty(result["answer-q1"])
+		assert.NotEmpty(result["answer-q1"])
+	})
 }
 
 func TestSetCommonValues(t *testing.T) {
 	assert := assert.New(t)
 
-	doc, err := getHTMLDocument("test-files/welcome-page.html")
-	assert.NoError(err)
-	values := make(url.Values)
+	forAllQuestionTypes(t, func(doc *html.Node, questionType string) {
+		values := make(url.Values)
 
-	err = setCommonValues(doc, values)
-	assert.NoError(err)
+		err := setCommonValues(doc, values)
+		assert.NoError(err)
 
-	result := flattenURLValues(values)
+		result := flattenURLValues(values)
 
-	t.Logf("%v\n", result)
+		t.Logf("%v\n", result)
 
-	assert.Equal("0", result["historyOrder"])
-	assert.Equal("032794ea-dfbb-4c33-95c2-2fbe5befd885", result["screenId"])
-	assert.Equal("Next", result["button-next"])
+		assert.Equal("0", result["historyOrder"])
+		assert.Equal("032794ea-dfbb-4c33-95c2-2fbe5befd885", result["screenId"])
+		assert.Equal("Next", result["button-next"])
+	})
 }
 
 func TestSetOpenSingleQuestionValues(t *testing.T) {
 	assert := assert.New(t)
 
-	doc, err := getHTMLDocument("test-files/singleline.html")
-	assert.NoError(err)
+	forBothTemplates(t, "alpha-single", func(doc *html.Node) {
+		values := make(url.Values)
 
-	values := make(url.Values)
+		err := setOpenSingleQuestionValues(doc, values)
+		assert.NoError(err)
 
-	err = setOpenSingleQuestionValues(doc, values)
-	assert.NoError(err)
+		result := flattenURLValues(values)
 
-	result := flattenURLValues(values)
+		t.Logf("%v\n", result)
 
-	t.Logf("%v\n", result)
-
-	assert.NotEmpty(result["answer-q1"])
+		assert.NotEmpty(result["answer-q1"])
+	})
 }
 
 func TestSetSingleCategoryValues(t *testing.T) {
 	assert := assert.New(t)
 
-	doc, err := getHTMLDocument("test-files/single-category.html")
-	assert.NoError(err)
+	forBothTemplates(t, "single-coded", func(doc *html.Node) {
+		values := make(url.Values)
 
-	values := make(url.Values)
+		err := setCategoryQuestionValues(doc, values)
+		assert.NoError(err)
 
-	err = setCategoryQuestionValues(doc, values)
-	assert.NoError(err)
+		result := flattenURLValues(values)
 
-	result := flattenURLValues(values)
+		t.Logf("%v\n", result)
 
-	t.Logf("%v\n", result)
+		answer1, err := strconv.ParseInt(result["answer-q1-m"], 0, 32)
+		assert.NoError(err)
 
-	answer1, err := strconv.ParseInt(result["answer-q1-m"], 0, 32)
-	assert.NoError(err)
-
-	assert.True(answer1 >= 0 && answer1 <= 3)
-	assert.Equal(fmt.Sprintf("q1-%d", answer1), result["answer-q1"])
+		assert.True(answer1 >= 0 && answer1 <= 5, "answer in range 1-5")
+		assert.Equal(fmt.Sprintf("q1-%d", answer1), result["answer-q1"])
+	})
 }
 
 func TestSetMultiCategoryValues(t *testing.T) {
 	assert := assert.New(t)
 
-	doc, err := getHTMLDocument("test-files/multi-category.html")
-	assert.NoError(err)
+	forBothTemplates(t, "multi-coded", func(doc *html.Node) {
+		values := make(url.Values)
 
-	values := make(url.Values)
+		err := setCategoryQuestionValues(doc, values)
+		assert.NoError(err)
 
-	err = setCategoryQuestionValues(doc, values)
-	assert.NoError(err)
+		answers := values["answer-q1-m"]
 
-	result := flattenURLValues(values)
+		assert.Len(answers, 4)
 
-	t.Logf("%v\n", result)
+		for _, answer := range answers {
+			answerInt, err := strconv.ParseInt(answer, 0, 32)
+			assert.NoError(err)
 
-	answer1, err := strconv.ParseInt(result["answer-q1-m"], 0, 32)
-	assert.NoError(err)
+			category := fmt.Sprintf("q1-%d", answerInt)
 
-	category := fmt.Sprintf("q1-%d", answer1)
-
-	assert.True(answer1 >= 0 && answer1 <= 3)
-	assert.Equal(category, result[fmt.Sprintf("answer-%s", category)])
+			assert.True(answerInt >= 1 && answerInt <= 10)
+			assert.Equal(category, values[fmt.Sprintf("answer-%s", category)][0])
+		}
+	})
 }
 
 func TestGetQuestionType(t *testing.T) {
 	assert := assert.New(t)
 
-	values := map[string]string{
-		"test-files/multi-category.html":  qTypeCategory,
-		"test-files/single-category.html": qTypeCategory,
-		"test-files/multiline.html":       qTypeOpenMulti,
-		"test-files/number.html":          qTypeNumber,
-		"test-files/singleline.html":      qTypeOpenSingle,
-		"test-files/welcome-page.html":    qTypePage,
-		"test-files/page-question.html":   qTypePage,
-	}
-
-	for file, questionType := range values {
-		doc, err := getHTMLDocument(file)
-		assert.NoError(err)
-
+	forAllQuestionTypes(t, func(doc *html.Node, questionType string) {
 		result := getQuestionType(doc)
 
-		assert.Equal(questionType, result, file)
-	}
+		assert.Equal(questionType, result)
+	})
 }
 
 func TestGetInterviewResponseReturnsErrOnSameHistoryOrder(t *testing.T) {
 	assert := assert.New(t)
 
-	doc, err := getHTMLString("test-files/single-category.html")
-	assert.NoError(err)
+	stringForAllQuestionTypes(t, func(doc string, _ string) {
+		_, _, err := getInterviewResponse(&doc, "0")
 
-	_, _, err = getInterviewResponse(&doc, "0")
-
-	assert.Error(err)
+		assert.Error(err)
+	})
 }
 
 func TestGetInterviewResponseReturnsGoodResponseForMultiLineText(t *testing.T) {
 	assert := assert.New(t)
 
-	doc, err := getHTMLString("test-files/multiline.html")
-	assert.NoError(err)
+	stringForBothTemplates(t, "open-multi", func(doc string) {
+		response, historyOrder, err := getInterviewResponse(&doc, "")
+		assert.NoError(err)
 
-	response, historyOrder, err := getInterviewResponse(&doc, "")
-	assert.NoError(err)
+		assert.Equal("0", historyOrder)
 
-	assert.Equal("0", historyOrder)
+		result := flattenURLValues(response)
+		t.Logf("%v\n", result)
 
-	result := flattenURLValues(response)
-	t.Logf("%v\n", result)
-
-	assert.NotEmpty(result["answer-q1"])
-	assert.True(len(result["answer-q1"]) > 10, "Length greater than 10")
+		assert.NotEmpty(result["answer-q1"])
+		assert.True(len(result["answer-q1"]) > 10, "Length greater than 10")
+	})
 }
 
 func TestGetInterviewResponseReturnsGoodResponseForSingleCategory(t *testing.T) {
 	assert := assert.New(t)
 
-	doc, err := getHTMLString("test-files/single-category.html")
-	assert.NoError(err)
+	stringForBothTemplates(t, "single-coded", func(doc string) {
+		response, historyOrder, err := getInterviewResponse(&doc, "")
+		assert.NoError(err)
 
-	response, historyOrder, err := getInterviewResponse(&doc, "")
-	assert.NoError(err)
+		assert.Equal("0", historyOrder)
 
-	assert.Equal("0", historyOrder)
+		result := flattenURLValues(response)
+		t.Logf("%v\n", result)
 
-	result := flattenURLValues(response)
-	t.Logf("%v\n", result)
+		answer1, err := strconv.ParseInt(result["answer-q1-m"], 0, 32)
+		assert.NoError(err)
 
-	answer1, err := strconv.ParseInt(result["answer-q1-m"], 0, 32)
-	assert.NoError(err)
-
-	assert.True(answer1 >= 0 && answer1 <= 3)
-	assert.Equal(fmt.Sprintf("q1-%d", answer1), result["answer-q1"])
+		assert.True(answer1 >= 1 && answer1 <= 5, "should be in range 1-5")
+		assert.Equal(fmt.Sprintf("q1-%d", answer1), result["answer-q1"])
+	})
 }
 
 func TestGetInterviewResponseReturnsGoodResponseForMultiCategory(t *testing.T) {
 	assert := assert.New(t)
 
-	doc, err := getHTMLString("test-files/multi-category.html")
-	assert.NoError(err)
+	stringForBothTemplates(t, "multi-coded", func(doc string) {
+		response, historyOrder, err := getInterviewResponse(&doc, "")
+		assert.NoError(err)
 
-	response, historyOrder, err := getInterviewResponse(&doc, "")
-	assert.NoError(err)
+		assert.Equal("0", historyOrder)
 
-	assert.Equal("0", historyOrder)
+		result := response["answer-q1-m"][0]
+		t.Logf("%v\n", result)
 
-	result := flattenURLValues(response)
-	t.Logf("%v\n", result)
+		answer1, err := strconv.ParseInt(result, 0, 32)
+		assert.NoError(err)
 
-	answer1, err := strconv.ParseInt(result["answer-q1-m"], 0, 32)
-	assert.NoError(err)
+		category := fmt.Sprintf("q1-%d", answer1)
 
-	category := fmt.Sprintf("q1-%d", answer1)
-
-	assert.True(answer1 >= 0 && answer1 <= 3)
-	assert.Equal(category, result[fmt.Sprintf("answer-%s", category)])
+		assert.True(answer1 >= 1 && answer1 <= 10, "answer should be in range 1-10")
+		assert.Equal(category, response[fmt.Sprintf("answer-%s", category)][0])
+	})
 }
 
 func TestGetInterviewResponseReturnsGoodResponseForSingleLineText(t *testing.T) {
 	assert := assert.New(t)
 
-	doc, err := getHTMLString("test-files/singleline.html")
-	assert.NoError(err)
+	stringForBothTemplates(t, "alpha-single", func(doc string) {
+		response, historyOrder, err := getInterviewResponse(&doc, "")
+		assert.NoError(err)
 
-	response, historyOrder, err := getInterviewResponse(&doc, "")
-	assert.NoError(err)
+		assert.Equal("0", historyOrder)
 
-	assert.Equal("0", historyOrder)
+		result := flattenURLValues(response)
+		t.Logf("%v\n", result)
 
-	result := flattenURLValues(response)
-	t.Logf("%v\n", result)
-
-	assert.NotEmpty(result["answer-q1"])
-	assert.True(len(result["answer-q1"]) > 4, "Length greater than 4")
-	assert.True(len(result["answer-q1"]) < 13, "Length smaller than 13")
+		assert.NotEmpty(result["answer-q1"])
+		assert.True(len(result["answer-q1"]) < 13, "Length smaller than 13")
+	})
 }
 
 func TestGetInterviewResponseReturnsGoodResponseForNumber(t *testing.T) {
 	assert := assert.New(t)
 
-	doc, err := getHTMLString("test-files/number.html")
-	assert.NoError(err)
+	stringForBothTemplates(t, "number", func(doc string) {
+		response, historyOrder, err := getInterviewResponse(&doc, "")
+		assert.NoError(err)
 
-	response, historyOrder, err := getInterviewResponse(&doc, "")
-	assert.NoError(err)
+		assert.Equal("0", historyOrder)
 
-	assert.Equal("0", historyOrder)
+		result := flattenURLValues(response)
+		t.Logf("%v\n", result)
 
-	result := flattenURLValues(response)
-	t.Logf("%v\n", result)
+		assert.NotEmpty(result["answer-q1"])
 
-	assert.NotEmpty(result["answer-q1"])
+		value, err := strconv.ParseInt(result["answer-q1"], 0, 32)
+		assert.NoError(err)
 
-	value, err := strconv.ParseInt(result["answer-q1"], 0, 32)
-	assert.NoError(err)
-
-	assert.True(value >= 5, "Value greater than 5")
-	assert.True(value <= 15, "Value smaller than 15")
+		assert.True(value >= 5, "Value greater than 5")
+		assert.True(value <= 15, "Value smaller than 15")
+	})
 }
 
 func TestGetInterviewResponseReturnsGoodResponseForWelcomePage(t *testing.T) {
 	assert := assert.New(t)
 
-	doc, err := getHTMLString("test-files/welcome-page.html")
-	assert.NoError(err)
+	stringForBothTemplates(t, "welcome-page", func(doc string) {
+		response, historyOrder, err := getInterviewResponse(&doc, "")
+		assert.NoError(err)
 
-	response, historyOrder, err := getInterviewResponse(&doc, "")
-	assert.NoError(err)
+		result := flattenURLValues(response)
 
-	result := flattenURLValues(response)
-
-	assert.Equal("0", historyOrder)
-	assert.Empty(result["answer-q1"])
-}
-
-func TestGetInterviewResponseReturnsGoodResponseForPageQuestion(t *testing.T) {
-	assert := assert.New(t)
-
-	doc, err := getHTMLString("test-files/page-question.html")
-	assert.NoError(err)
-
-	response, historyOrder, err := getInterviewResponse(&doc, "")
-	assert.NoError(err)
-
-	result := flattenURLValues(response)
-
-	assert.Equal("0", historyOrder)
-	assert.Empty(result["answer-q1"])
+		assert.Equal("0", historyOrder)
+		assert.Empty(result["answer-q1"])
+	})
 }
